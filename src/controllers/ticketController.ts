25771eaa2fd019ae967a72687881d440247f5ff9
Paul Markus <paulmarkus14@yahoo.com>
2024-02-27 00:58:20 +0200

    Fixed error 404

--- src/controllers/ticketController.ts
+++ src/controllers/ticketController.ts
@@ -40,11 +40,15 @@ class TicketController {
     public async getFrequency(req: Request, res: Response): Promise<void> {
         try {
             const tickets = await tiketDAO.getAllNumbers();
-            const extractedNumbers = tickets.flatMap((ticket: { numbers: any[]; }) =>
-                ticket.numbers.flatMap((arr: any) => arr)
-            );
-            const frequency = calculateFrequency(extractedNumbers);
-            res.json(frequency);
+            if (tickets.length > 0) {
+                const extractedNumbers = tickets.flatMap((ticket: { numbers: any[]; }) =>
+                    ticket.numbers.flatMap((arr: any) => arr)
+                );
+                const frequency = calculateFrequency(extractedNumbers);
+                res.status(200).json(frequency);
+            } else {
+                res.status(404).json({ message: 'Tickets not found' });
+            }
         } catch (error) {
             res.status(500).json({ message: 'Internal server error' });
         }
